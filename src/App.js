31006dd5e50f12fab2328894e7f0d71e8fb54bfc
mimiprosper <mimiprosper@hotmail.com>
2023-09-12 21:11:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Me from './images/me.jpg';
+
+function App() {
+  return (
+    <div className="profile">
+      <img src={Me} alt="" />
+      <h1>My Profile</h1>
+      <p>
+        My name is Emmanuel Okeke, am a Full Stack
+        Developer with 6+ years of hands-on experience designing, developing,
+        and implementing applications and solutions using a range of
+        technologies and programming languages. Seeking to leverage broad
+        development experience and hands-on technical expertise in a challenging
+        role as a Full Stack Developer.
+      </p>
+    </div>
+  );
+}
+
+export default App;
